5c9d6af5341f5f6c3cb0fc23b36f3103a6259cd0
Louis Wan <louiswan@astri.org>
2017-05-08 15:19:32 +0800

    finalized

--- test.py
+++ test.py
@@ -196,16 +196,76 @@ samples_100 = len(X_train)
 #     for i in xrange(3):
 #         print results[res][i]
 
+# TODO: Import 'GridSearchCV', 'make_scorer', and any other necessary libraries
+from sklearn.linear_model import LogisticRegression
 from sklearn.grid_search import GridSearchCV
-from sklearn.metrics import make_scorer
-
-logit_clf = LogisticRegression()
-print logit_clf.get_params().keys()
+from sklearn.metrics import make_scorer, fbeta_score
 
 tran_y_train = [0 if i[0] < 1 else 1 for i in y_train.values.tolist()]
+tran_y_test = [0 if i[0] < 1 else 1 for i in y_test.values.tolist()]
 
-parameters = {'C':[float(i)/10 for i in range(1, 11)],
+# TODO: Initialize the classifier
+clf = LogisticRegression()
+
+# TODO: Create the parameters list you wish to tune
+parameters = {'C':[float(i)/5 for i in range(1, 11)],
               'solver':['newton-cg', 'lbfgs', 'liblinear', 'sag']}
 
-clf = GridSearchCV(logit_clf, parameters)
-clf.fit(X_train, tran_y_train)
+# TODO: Make an fbeta_score scoring object
+scorer = make_scorer(fbeta_score, beta=2)
+
+# TODO: Perform grid search on the classifier using 'scorer' as the scoring method
+grid_obj = GridSearchCV(clf, param_grid=parameters,
+                              scoring=scorer)
+
+# TODO: Fit the grid search object to the training data and find the optimal parameters
+grid_fit = grid_obj.fit(X_train, tran_y_train)
+
+# Get the estimator
+best_clf = grid_fit.best_estimator_
+
+# Make predictions using the unoptimized and model
+predictions = (clf.fit(X_train, tran_y_train)).predict(X_test)
+best_predictions = best_clf.predict(X_test)
+y_test = tran_y_test
+
+print "Unoptimized model\n------"
+print "Accuracy score on testing data: {:.4f}".format(accuracy_score(y_test, predictions))
+print "F-score on testing data: {:.4f}".format(fbeta_score(y_test, predictions, beta = 0.5))
+print "\nOptimized Model\n------"
+print "Final accuracy score on the testing data: {:.4f}".format(accuracy_score(y_test, best_predictions))
+print "Final F-score on the testing data: {:.4f}".format(fbeta_score(y_test, best_predictions, beta = 0.5))
+
+print len(best_clf.coef_[0])
+
+from sklearn.tree import DecisionTreeClassifier
+
+# TODO: Import a supervised learning model that has 'feature_importances_'
+clf = DecisionTreeClassifier()
+
+# TODO: Train the supervised model on the training set
+model = clf.fit(X_train, tran_y_train)
+
+# TODO: Extract the feature importances
+importances = model.feature_importances_
+
+# Import functionality for cloning a model
+from sklearn.base import clone
+
+# Reduce the feature space
+X_train_reduced = X_train[X_train.columns.values[(np.argsort(importances)[::-1])[:5]]]
+X_test_reduced = X_test[X_test.columns.values[(np.argsort(importances)[::-1])[:5]]]
+
+# Train on the "best" model found from grid search earlier
+clf = (clone(best_clf)).fit(X_train_reduced, tran_y_train)
+
+# Make new predictions
+reduced_predictions = clf.predict(X_test_reduced)
+
+# Report scores from the final model using both versions of data
+print "Final Model trained on full data\n------"
+print "Accuracy on testing data: {:.4f}".format(accuracy_score(y_test, best_predictions))
+print "F-score on testing data: {:.4f}".format(fbeta_score(y_test, best_predictions, beta = 0.5))
+print "\nFinal Model trained on reduced data\n------"
+print "Accuracy on testing data: {:.4f}".format(accuracy_score(y_test, reduced_predictions))
+print "F-score on testing data: {:.4f}".format(fbeta_score(y_test, reduced_predictions, beta = 0.5))
